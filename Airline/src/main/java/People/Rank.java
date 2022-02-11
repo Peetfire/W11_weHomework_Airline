c80792fbb08a8da6620d401cb4280227da9c2fdb
petermordaunt <peetfire@hotmail.com>
2022-02-11 15:14:23 +0000

    Testing - test created for CabinCrewMember and Rank classes.

---
+++ Airline/src/main/java/People/Rank.java
@@ -0,0 +1,15 @@
+package People;
+
+public enum Rank {
+    PILOT,
+    CO_PILOT,
+    ENGINEER,
+    CABIN_SERVICE_DIRECTOR,
+    PURSER,
+    GRADE1,
+    GRADE2,
+    AIR_STEWARD,
+    AIR_HOSTESS,
+    TRAINEE
+
+}
--- Airline/src/test/java/PeopleTests/CabinCrewMemberTest.java
+++ Airline/src/test/java/PeopleTests/CabinCrewMemberTest.java
@@ -1,13 +1,27 @@
 package PeopleTests;
 
 import People.CabinCrewMember;
+import People.Rank;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CabinCrewMemberTest {
     CabinCrewMember crewMember;
 
     @Before
     public void before(){
-        crewMember = new CabinCrewMember("Brian Dowling", "")
+        crewMember = new CabinCrewMember("Brian Dowling", Rank.PURSER);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Brian Dowling", crewMember.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.PURSER, crewMember.getRank());
     }
 }
