0bec8ce7f2007628468c9f490cdd643134488521
petermordaunt <peetfire@hotmail.com>
2022-02-11 15:18:29 +0000

    Testing - all tests passed for CabinCrewMemberTest

--- Airline/src/main/java/People/CabinCrewMember.java
+++ Airline/src/main/java/People/CabinCrewMember.java
@@ -1,4 +1,18 @@
 package People;
 
-public class CabinCrewMember {
+public class CabinCrewMember extends Person {
+    Rank rank;
+
+    public CabinCrewMember(String name, Rank rank) {
+        super(name);
+        this.rank = rank;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
 }
