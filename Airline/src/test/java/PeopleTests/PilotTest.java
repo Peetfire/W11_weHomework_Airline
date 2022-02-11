25fd7835cbfcc7254f27fe9a798faa8935fb5922
petermordaunt <peetfire@hotmail.com>
2022-02-11 16:03:40 +0000

    Testing - added tests for pilotTest: hasName, hasRank, hasLicence.

--- Airline/src/test/java/PeopleTests/PilotTest.java
+++ Airline/src/test/java/PeopleTests/PilotTest.java
@@ -1,4 +1,35 @@
 package PeopleTests;
 
+import People.Pilot;
+import People.Rank;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot("Amelia Earhart", Rank.PILOT, "PL-1897 07 24");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Brian Dowling", pilot.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(Rank.PURSER, pilot.getRank());
+    }
+
+    @Test
+    public void hasLicence(){
+        assertEquals("PL-1897 07 24", pilot.getLicence());
+    }
+
+
 }
