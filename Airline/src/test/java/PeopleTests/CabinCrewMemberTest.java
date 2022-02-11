46edbf38c236bd0d599d3725fd9da410f688c74a
petermordaunt <peetfire@hotmail.com>
2022-02-11 15:07:29 +0000

    planning - updated class diagram, adding enum for rank types.

--- Airline/src/test/java/PeopleTests/CabinCrewMemberTest.java
+++ Airline/src/test/java/PeopleTests/CabinCrewMemberTest.java
@@ -1,4 +1,13 @@
 package PeopleTests;
 
+import People.CabinCrewMember;
+import org.junit.Before;
+
 public class CabinCrewMemberTest {
+    CabinCrewMember crewMember;
+
+    @Before
+    public void before(){
+        crewMember = new CabinCrewMember("Brian Dowling", "")
+    }
 }
